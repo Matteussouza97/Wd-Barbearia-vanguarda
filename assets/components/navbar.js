8ec890fc7da4889cd6e42857faeb1de45c15ae68
Mateus Andrade <Pagogosouza97@gmail.com>
2023-07-21 11:44:45 -0300

    Versão B-23J23

--- assets/components/navbar.js
+++ assets/components/navbar.js
@@ -4,13 +4,41 @@ export class Navbar extends LitElement {
     static styles = [
         css`
             :host {
-                display: block;
+                display: flex;
+                justify-content:space-between;
+                align-items:center;
+                padding:0 2rem;
+                height: 96px;
+                width:100%;
+
+                position:fixed;
+                top:0;
+
+                box-sizing:border-box;
+                z-index:100;
+
+                mix-blend-mode:difference;
+                color:white;
+
+                transition: top 1s;
+            }
+
+            app-logo{
+                background-color:white;
+            }
+
+            feather-icon {
+                cursor:pointer;
             }
         `
     ];
 
     render() {
-        return html``;
+        return html`
+        <app-logo></app-logo>
+        <feather-icon icon="menu"></feather-icon>
+        
+        `;
     }
 }
 customElements.define('app-navbar', Navbar);
