6c2365508422bc5e84dfe5bbad51317ccc056ac4
Mateus Andrade <Pagogosouza97@gmail.com>
2023-07-28 09:43:12 -0300

    criação do slide dos mapas

--- assets/sections/unidades-section.js
+++ assets/sections/unidades-section.js
@@ -6,9 +6,9 @@ export class UnidadesSection extends LitElement {
     section,
     css`
       :host {
-        height:fit-content;
+        height: fit-content;
         gap: 32px;
-        padding:0 0 4rem 0;
+        padding: 0 0 4rem 0;
       }
 
       app-paragrafo {
@@ -25,40 +25,80 @@ export class UnidadesSection extends LitElement {
         position: absolute;
         left: -64px;
         top: -56px;
-        z-index:-10;
-        --cor-bolinhas:var(--cor-tom-3)
+        z-index: -10;
+        --cor-bolinhas: var(--cor-tom-3);
       }
 
-      swiper-container{
-        width:100%;
-        height:300px;
+      swiper-container {
+        width: 100svw;
+        height: 300px;
+        position:relative;
+        left:50%;
+        transform:translatex(-50%);
       }
-      swiper-slide{
-        display:flex;
-        justify-content:center;
-        padding-top:1rem;
-        transition:transform 500ms;
+      swiper-slide {
+        display: flex;
+        justify-content: center;
+        padding-top: 1rem;
+        transition: transform 500ms;
       }
 
-      .swiper-slide-active{
-        transform:scale(1.1);
+      .swiper-slide-active {
+        transform: scale(1.1);
       }
-
 
       /* responsivo tablet */
-      @media (min-width: 768px){
-        app-paragrafo{
-          width:100%;
-          max-width:500px;
-          text-align:center;
+      @media (min-width: 768px) {
+        app-paragrafo {
+          width: 100%;
+          max-width: 500px;
+          text-align: center;
         }
-        app-mapa{
-          width:400px;
-          height:300px;
+        app-mapa {
+          width: 400px;
+          height: 300px;
+        }
+   
+        swiper-container{
+          height:400px;
         }
       }
     `,
   ];
+       
+  firstUpdated() {
+    const swiper = this.renderRoot.querySelector('swiper-container')
+
+    console.log(swiper)
+
+    Object.assign(swiper, {
+      spaceBetween:-75,
+      initalSlide: 1,
+      rewind: true,
+      breakpoints:{
+        768: {
+          spaceBetween: -275
+        },
+        1024: {
+          spaceBetween:  -575
+        },
+        1368: {
+         slidesPerView: 3,
+          spaceBetween: 0,
+          centeredSlides: true,
+        },
+      }
+    })
+
+
+    swiper.initialize()
+  }
+
+
+
+
+
+
 
   render() {
     return html`
@@ -67,21 +107,23 @@ export class UnidadesSection extends LitElement {
         unidades, estão abertas todos os dias. <br /><br />Confira os horários
         específicos de cada unidade.</app-paragrafo
       >
-      <swiper-container space-between="-75">
+      <swiper-container init="false">
         <swiper-slide>
           <app-mapa></app-mapa>
         </swiper-slide>
         <swiper-slide>
-          <app-mapa></app-mapa>
+          <app-mapa>
+            <img slot="imagem" loading="lazy" src="mapa-2.jpg" alt="filial 2"/>
+          </app-mapa>
         </swiper-slide>
         <swiper-slide>
-          <app-mapa></app-mapa>
+          <app-mapa><img slot="imagem" loading="lazy" src="mapa-3.jpg" alt="filial 3"/></app-mapa>
         </swiper-slide>
         <swiper-slide>
-          <app-mapa></app-mapa>
+          <app-mapa><img slot="imagem" loading="lazy" src="mapa-4.jpg" alt="filial 4"/></app-mapa>
         </swiper-slide>
         <swiper-slide>
-          <app-mapa></app-mapa>
+          <app-mapa><img slot="imagem" loading="lazy" src="mapa-2.jpg" alt="filial"/></app-mapa>
         </swiper-slide>
       </swiper-container>
       <app-bolinhas formato="fila-metade"></app-bolinhas>
